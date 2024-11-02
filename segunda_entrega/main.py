4c62dc87871ea06c0675b499c52663750d5fa029
Nicol_O <nicol.ortiz@correounivalle.edu.co>
2024-11-01 22:43:41 -0500

    better description

--- segunda_entrega/main.py
+++ segunda_entrega/main.py
@@ -1,18 +1,24 @@
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 import os
+import time  
 
 # Crear la carpeta outputs si no existe
 output_dir = "outputs"
 os.makedirs(output_dir, exist_ok=True)
 
 # Lista de notebooks que quieres ejecutar
-notebooks = ["dim_cliente.ipynb", 
-             "dim_mensajero.ipynb", 
-             "dim_tiempo.ipynb", 
-             "dim_descripcion_novedad.ipynb",
-             "hecho_novedades_servicio.ipynb",
-             "hecho_servicio_mensual.ipynb"]
+notebooks = [
+    "dim_cliente.ipynb",
+    "dim_mensajero.ipynb",
+    "dim_tiempo.ipynb",
+    "dim_descripcion_novedad.ipynb",
+    "hecho_novedades_servicio.ipynb",
+    "hecho_servicio_mensual.ipynb"
+]
+
+# Iniciar el temporizador
+start_time = time.time()
 
 for notebook in notebooks:
     with open(notebook) as f:
@@ -20,7 +26,14 @@ for notebook in notebooks:
         ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
         ep.preprocess(nb)
 
-        # Guardar el notebook ejecutado
+        # Guardar el notebook ejecutado en la carpeta outputs
         output_path = os.path.join(output_dir, f"output_{notebook}")
         with open(output_path, 'w') as f:
             nbformat.write(nb, f)
+
+# Calcular el tiempo total de ejecución
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Todos los notebooks han sido ejecutados y los outputs se han guardado en la carpeta 'outputs'.")
+print(f"Tiempo total de ejecución: {elapsed_time:.2f} segundos.")
