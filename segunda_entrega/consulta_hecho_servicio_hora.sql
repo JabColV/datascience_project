63afdf919678a4dee24042d0d2a50f8e7a12c33e
davashikashi <deison.cardona@correounivalle.edu.co>
2024-11-08 14:09:06 -0500

    todo hecho y por revisar
    :D

--- segunda_entrega/consulta_hecho_servicio_hora.sql
+++ segunda_entrega/consulta_hecho_servicio_hora.sql
@@ -1,17 +1,7 @@
 -- a que hora estan más ocupados los mensajeros
-SELECT hora, COUNT(*) AS cantidad_solicitudes
+SELECT hora_solicita, COUNT(*) AS cantidad_solicitudes
 FROM public.hecho_servicio_hora
-GROUP BY hora
-ORDER BY cantidad_solicitudes DESC, hora;
+GROUP BY hora_solicita
+ORDER BY cantidad_solicitudes DESC, hora_solicita;
 
 
---agrupando por solicitudes y horas
-
-SELECT hora, SUM(cantidad_solicitudes) AS total_solicitudes
-FROM (
-    SELECT hora, key_mensajero, COUNT(*) AS cantidad_solicitudes
-    FROM public.hecho_servicio_hora
-    GROUP BY hora, key_mensajero
-) AS subquery
-GROUP BY hora
-ORDER BY hora;
