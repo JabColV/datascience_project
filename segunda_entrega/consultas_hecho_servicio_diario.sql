c3aeb67a48abda2d935ddda44ddd8024909510d6
Nicol Ortiz <nicol.ortiz@correounivalle.edu.co>
2024-11-10 15:41:37 -0500

    Remaining detail in facts

--- segunda_entrega/consultas_hecho_servicio_diario.sql
+++ segunda_entrega/consultas_hecho_servicio_diario.sql
@@ -1,5 +1,12 @@
 -- ¿Cuales son los días donde más solicitudes hay? --
-SELECT key_tiempo, solicitud_por_dia AS total_solicitudes_por_dia
+SELECT key_fecha, solicitud_por_dia AS total_solicitudes_por_dia
 	FROM public.hecho_servicio_dia
-	GROUP BY key_tiempo, solicitud_por_dia
+	GROUP BY key_fecha, solicitud_por_dia
 	ORDER BY total_solicitudes_por_dia DESC;
+
+-- Número de servicios solicitados por mensajero y por dia --
+SELECT key_mensajero, 
+           key_fecha, 
+           cantidad_solicitud_por_dia_mensajero AS total_solicitud_por_dia_mensajero
+    FROM public.hecho_servicio_dia
+    GROUP BY key_mensajero, key_fecha, cantidad_solicitud_por_dia_mensajero
--- segunda_entrega/consultas_hecho_servicio_mensual.sql
+++ segunda_entrega/consultas_hecho_servicio_mensual.sql
@@ -1,34 +1,36 @@
 -- ¿En qué meses del año los clientes solicitan más servicios de mensajería? --
-SELECT key_tiempo, solicitud_por_mes AS total_solicitudes_por_mes
+SELECT key_fecha, solicitud_por_mes AS total_solicitudes_por_mes
 	FROM public.hecho_servicio_mensual
-	GROUP BY key_tiempo, solicitud_por_mes
+	GROUP BY key_fecha, solicitud_por_mes
 	ORDER BY total_solicitudes_por_mes DESC;
 
 -- Número de servicios solicitados por cliente y por mes --
-SELECT key_cliente, key_tiempo, solicitud_por_mes_cliente 
+SELECT key_cliente, key_fecha, solicitud_por_mes_cliente 
 FROM public.hecho_servicio_mensual
-GROUP BY key_cliente, key_tiempo, solicitud_por_mes_cliente;
+GROUP BY key_cliente, key_fecha, solicitud_por_mes_cliente;
 
 -- Número de servicios solicitados por X cliente en los diversos meses --
-SELECT key_cliente, key_tiempo, solicitud_por_mes_cliente
+SELECT key_cliente, key_fecha, solicitud_por_mes_cliente
 FROM public.hecho_servicio_mensual
 WHERE key_cliente=8
-GROUP BY key_cliente, key_tiempo, solicitud_por_mes_cliente;
+GROUP BY key_cliente, key_fecha, solicitud_por_mes_cliente;
 
 -- Mensajeros más eficientes (Los que más servicios prestan) --
-SELECT key_mensajero, cantidad_solicitud_por_mensajero AS total_solicitud_por_mes_mensajero
-FROM public.hecho_servicio_mensual
-GROUP BY key_mensajero, cantidad_solicitud_por_mensajero
-ORDER BY cantidad_solicitud_por_mensajero DESC;
+SELECT key_mensajero, 
+       SUM(total_solicitud_por_mes_mensajero) AS total_solicitudes
+FROM (
+    SELECT key_mensajero, 
+           key_fecha, 
+           cantidad_solicitud_por_mes_mensajero AS total_solicitud_por_mes_mensajero
+    FROM public.hecho_servicio_mensual
+    GROUP BY key_mensajero, key_fecha, cantidad_solicitud_por_mes_mensajero
+) AS subquery
+GROUP BY key_mensajero
+ORDER BY total_solicitudes DESC;
 
 -- Para saber si hay datos duplicados --
-SELECT key_tiempo, key_cliente, key_mensajero, solicitud_por_mes_cliente, cantidad_solicitud_por_mensajero, COUNT(*) AS conteo
+SELECT key_fecha, key_cliente, key_mensajero, solicitud_por_mes_cliente, cantidad_solicitud_por_mes_mensajero, COUNT(*) AS conteo
 FROM hecho_servicio_mensual
-GROUP BY key_tiempo, key_cliente, key_mensajero, solicitud_por_mes_cliente, cantidad_solicitud_por_mensajero
+GROUP BY key_fecha, key_cliente, key_mensajero, solicitud_por_mes_cliente, cantidad_solicitud_por_mes_mensajero
 HAVING COUNT(*) > 1;
 
--- ¿Cuáles son las sedes que más servicios solicitan por cada cliente? --
--- SELECT key_sede, key_cliente, COUNT(solicitud_por_mes_sede) AS total_solicitudes --
---	FROM public.hecho_servicio_mensual --
---	GROUP BY key_sede, key_cliente --
---	ORDER BY total_solicitudes DESC; --
