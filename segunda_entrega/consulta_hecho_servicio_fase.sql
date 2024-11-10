5fb48981a6af5f964038116b2d87a96975461ffb
Nicol Ortiz <nicol.ortiz@correounivalle.edu.co>
2024-11-10 16:43:48 -0500

    Last requests for fact accumulating

--- segunda_entrega/consulta_hecho_servicio_fase.sql
+++ segunda_entrega/consulta_hecho_servicio_fase.sql
@@ -1,19 +1,46 @@
---timepo promedio general de entrega
+--tiempo promedio general de entrega
 WITH servicio_unico AS (
-    SELECT DISTINCT ON (servicio_id) 
-        servicio_id,
-        tiempo_promedio_dias,
-        tiempo_promedio_horas
+    SELECT DISTINCT ON (id) 
+        id,
+        promedio_entrega_general_dias,
+        promedio_entrega_general_horas
     FROM 
-        hecho_servicio_fase
+        hecho_accumulating_snapshot
     ORDER BY 
-        servicio_id
+        id
 )
-
 SELECT 
-    AVG(tiempo_promedio_dias) AS tiempo_promedio_entrega_dias,
-    AVG(tiempo_promedio_horas) AS tiempo_promedio_entrega_horas
+    AVG(promedio_entrega_general_dias) AS promedio_entrega_general_dias,
+    AVG(promedio_entrega_general_horas) AS promedio_entrega_general_horas
 FROM 
     servicio_unico;
 
 
+-- Tiempo de promedio de duración por cada fase
+WITH servicio_unico AS (
+    SELECT DISTINCT ON (id) 
+        id,
+		dias_mensajero_asignado,
+		horas_mensajero_asignado,
+		dias_recogido_mensajero,
+		horas_recogido_mensajero,
+		dias_entregado_destino,
+		horas_entregado_destino,
+		dias_terminado,
+	    horas_terminado
+    FROM 
+        hecho_accumulating_snapshot
+    ORDER BY 
+        id
+)
+SELECT 
+    AVG(dias_mensajero_asignado) AS promedio_dias_mensajero_asignado,
+	AVG(horas_mensajero_asignado) AS promedio_horas_mensajero_asignado,
+	AVG(dias_recogido_mensajero) AS promedio_dias_recogido_mensajero,
+	AVG(horas_recogido_mensajero) AS promedio_horas_recogido_mensajero,
+	AVG(dias_entregado_destino) AS promedio_dias_entregado_destino,
+	AVG(horas_entregado_destino) AS promedio_horas_entregado_destino,
+	AVG(dias_terminado) AS promedio_dias_terminado,
+    AVG(horas_terminado) AS promedio_horas_terminado
+FROM 
+    servicio_unico;
--- segunda_entrega/consultas_hecho_novedades_servicio.sql
+++ segunda_entrega/consultas_hecho_novedades_servicio.sql
@@ -1,3 +1,4 @@
+-- ¿Cuales son las novedades que más se presentan durante la prestación del servicio?
 SELECT key_descripcion_novedad, cantidad_por_descripcion_novedad AS total_cantidad
 	FROM public.hecho_novedad_servicio
 	GROUP BY key_descripcion_novedad, cantidad_por_descripcion_novedad
